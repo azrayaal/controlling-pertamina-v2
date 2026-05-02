8acf78dd15375fcb43b16ba628ab4f2a15c58b85
Azra Yazid <azrayazidalkautsar@gmail.com>
2026-05-02 23:55:09 +0700

    fix sidebar

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({ children, title, subtitle }: Dashboard
   return (
     <div className="flex h-screen overflow-hidden" style={{ background: "#ECECEC" }}>
       <Sidebar />
-      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden ml-[19px]">
         <Header title={title} subtitle={subtitle} />
         <main className="flex-1 overflow-y-auto p-4" style={{ background: "#ECECEC" }}>
           {children}
